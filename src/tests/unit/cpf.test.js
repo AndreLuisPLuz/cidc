82aee2e662be7222f0ac91907767758be663b92b
AndreLuisPLuz <andreluispluz@gmail.com>
2024-04-24 15:45:30 -0300

    Desafio: validação de CPF

---
+++ src/tests/unit/cpf.test.js
@@ -0,0 +1,23 @@
+import { validateCpf } from "../../common/validateCpf";
+
+describe("CPF", () => {
+    it("Se o CPF for válido, deve retornar verdadeiro.", () => {
+        const res = validateCpf("08363535958");
+        expect(res).toBe(true);
+    });
+
+    it("Se o CPF for inválido, deve retornar falso.", () => {
+        const res = validateCpf("08363535959");
+        expect(res).toBe(false);
+    });
+
+    it("Se o CPF for inválido, deve retornar falso.", () => {
+        const res = validateCpf("22212222222");
+        expect(res).toBe(false);
+    });
+
+    it("Se o CPF for inválido, deve retornar verdadeiro.", () => {
+        const res = validateCpf("08513060984");
+        expect(res).toBe(true);
+    });
+});
