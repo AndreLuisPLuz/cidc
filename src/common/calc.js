936288146d7b6613e63aa9c2d405a525fe5e625a
AndreLuisPLuz <andreluispluz@gmail.com>
2024-04-24 15:07:09 -0300

    Testes para calculadora completos.

--- src/common/calc.js
+++ src/common/calc.js
@@ -1,9 +1,25 @@
-function Sum(a, b) {
+const Sum = (a, b) => {
     return a + b;
 }
 
-function Sub(a, b) {
+const Sub = (a, b) => {
     return a - b;
 }
 
-module.exports = { Sum, Sub };
+const Multiplication = (a, b) => {
+    return a * b;
+};
+
+const Division = (a, b) => {
+    if (b == 0)
+        throw new RangeError("The divisor cannot be zero.");
+
+    return a / b;
+}
+
+export {
+    Sum,
+    Sub,
+    Multiplication,
+    Division
+};
---
+++ src/tests/unit/calc.test.js
@@ -0,0 +1,34 @@
+import {
+    Sum,
+    Sub,
+    Multiplication,
+    Division
+} from "../../common/calc";
+
+describe("Calculadora", () => {
+    it("Deve retornar o resultado da soma de 1 + 2", () => {
+        const res = Sum(1, 2);
+        expect(res).toBe(3);
+    });
+
+    it("Deve retornar o resultado da subtração de 1 - 2", () => {
+        const res = Sub(1, 2);
+        expect(res).toBe(-1);
+    });
+
+    it("Deve retornar a multiplicação de 3 * 1", () => {
+        const res = Multiplication(3, 1);
+        expect(res).toBe(3);
+    });
+
+    it("Em uma divisão com ambos os valores diferentes de zero, deve retornar o resultado.", () => {
+        const res = Division(10, 2);
+        expect(res).toBe(5);
+    });
+
+    it("Em uma divisão onde o divisor seja igual a zero, deve levantar uma exceção.", () => {
+        expect(() => {
+            Division(10, 0);
+        }).toThrow(RangeError);
+    });
+});
